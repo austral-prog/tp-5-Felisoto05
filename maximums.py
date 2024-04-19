3b31d8339bc4a24b1a0e95978e4b07faee0aec94
Felisoto05 <162723327+Felisoto05@users.noreply.github.com>
2024-04-19 15:26:22 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,12 +1,10 @@
 def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
     if x>y: 
         return x 
     else:
         return y 
 
 def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
     if x>y and x>z:
         return x   
     elif x<y and x>z:
